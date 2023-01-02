f6c11b962d88b052adcf852b5afdf743516abe4f
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-02 10:07:21 +0100

    Basics

---
+++ src/routes/routes.js
@@ -0,0 +1,15 @@
+import Login from "../pages/Login/Login"
+import Home from "../pages/Home/Home"
+
+const routes = [
+    {
+        path : '/',
+        component : Login
+    },
+    {
+        path : '/home',
+        component : Home
+    },
+]
+
+export default routes
