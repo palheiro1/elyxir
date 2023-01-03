a3190b6ae8f58b3091ae3ad2d386e3ddf072b704
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-03 19:01:53 +0100

    Fix handlers

--- src/components/LoginPage/UserRestore/UserRestore.js
+++ src/components/LoginPage/UserRestore/UserRestore.js
@@ -1,10 +1,17 @@
-import { Box, Button, FormControl, FormHelperText, FormLabel, Input, InputGroup, InputRightAddon, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, FormControl, FormHelperText, FormLabel, Input, Stack, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
 const UserRestore = () => {
 
     const [ name, setName ] = useState("");
     const [ account, setAccount ] = useState("");
+    const [ passphrase, setPassphrase ] = useState("");
+    const [ pin, setPin ] = useState("");
+
+    const handleChangeName = (e) => setName(e.target.value);
+    const handleChangeAccount = (e) => setAccount(e.target.value);
+    const handleChangePassphrase = (e) => setPassphrase(e.target.value);
+    const handleChangePin = (e) => setPin(e.target.value);
 
     return(
         <Box>
@@ -14,26 +21,26 @@ const UserRestore = () => {
 
             <Stack direction={'row'}>
                 <FormControl variant="floating" id="name">
-                    <Input placeholder=" " value={name} size="lg"/>
+                    <Input placeholder=" " value={name} size="lg" onChange={handleChangeName}/>
                     <FormLabel>Name</FormLabel>
                     <FormHelperText textAlign="center">Used to identify you on this device</FormHelperText>
                 </FormControl>
 
                 <FormControl variant="floating" id="pin">
-                    <Input placeholder=" " value={account} size="lg"/>
+                    <Input placeholder=" " value={account} size="lg" onChange={handleChangeAccount}/>
                     <FormLabel>Account</FormLabel>
                     <FormHelperText textAlign="center">Ardor Account ID</FormHelperText>
                 </FormControl>
             </Stack>
 
             <FormControl variant="floating" id="account" my={6}>
-                <Input placeholder=" " minH="100px" />
+                <Input placeholder=" " minH="100px" value={passphrase} onChange={handleChangePassphrase} />
                 <FormLabel>Passphrase</FormLabel>
             </FormControl>
 
             <Stack direction={'row'}>
                 <FormControl variant="floating" id="name">
-                    <Input placeholder=" " value={name} size="lg"/>
+                    <Input placeholder=" " value={pin} size="lg" onChange={handleChangePin} />
                     <FormLabel>PIN</FormLabel>
                     <FormHelperText textAlign="center">Used to access easily</FormHelperText>
                 </FormControl>
