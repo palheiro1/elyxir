4796dde81eb4c5631282ffb41ef2b25412d04305
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-03 11:09:17 +0100

    Update validators.js

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,72 +1,11 @@
 import { secretPhraseToAccountId } from 'ardorjs';
 
-/*
-function validateQuantity(value, max, min, status) {
-
-    const { invalid, error } = status;
-    const val_num = Number(value);
-
-    if (value!==''){
-        invalid = !(value.match('[1-9]\d*|0\d+'));//doesnt work with MUI TextField
-        error = invalid ? value+' is not a number.' : '';
-    if (val_num<=max && val_num >= min){
-      // its a number!
-      if (val_num%1!==0){
-          return {invalid:true, error:'You can only buy whole packs'};
-      }
-      return {invalid:invalid, error:error};
-    }
-    else if (val_num>max) {
-      invalid = true;
-      error=value+' is over the maximum amount ('+max+').';
-      return {invalid:invalid, error:error};
-    }
-    else if (val_num<min){
-      invalid = true;
-      error=value+' is below the minimum amount ('+min+').';
-      return {invalid:invalid, error:error};
-    }
-  }
-  else {
-    invalid = true;
-    error = 'invalid value';
-  }
-  return {invalid:invalid, error:error};
-}
-*/
-
-/*
-function validatePrice(value, max, min, status) {
-  let invalid=status.invalid;
-  let error=status.error;
-  let val_num = Number(value);
-  //console.log(value,typeof(value),val_num,max,min,invalid,error);
-  if (value!==''){
-    invalid = !(value.match('[1-9]\d*|0\d+')); //note: this doesnt work with the MUI-textfield 
-    error = invalid ? value+' is not a number.' : '';
-    if (val_num<=max && val_num >= min){
-      return {invalid:invalid, error:error};
-    }
-    else if (val_num>max) {
-      invalid = true;
-      error=value+' is over the maximum of '+max+' Ignis.';
-      return {invalid:invalid, error:error};
-    }
-    else if (val_num<min){
-      invalid = true;
-      error=value+' is below the minimum of '+min+' Ignis.';
-      return {invalid:invalid, error:error};
-    }
-  }
-  else {
-    invalid = true;
-    error = 'invalid value'; //non-numbers will be filtered and can cause empty string.
-  }
-  return {invalid:invalid, error:error};
-}
-*/
-
-
+/**
+ * @param {string} value - user name
+ * @param {array} userList - list of all users
+ * @returns {object} - object with invalid and error properties
+ * @description - checks if the user name is already taken
+ */
 export function validateUsername(value, userList) {
 
     const user_existing = userList.find((el)=> el === value);
@@ -81,7 +20,12 @@ export function validateUsername(value, userList) {
     return {invalid:false, error:""};
 }
 
-
+/**
+ * @param {string} value - user name
+ * @param {string} account - account address
+ * @returns {object} - object with invalid and error properties
+ * @description - checks if the pass phrase belongs to the account
+ */
 export function validatePassPhrase(value, account){
     let invalid=false;
     let error=""; 
@@ -94,29 +38,12 @@ export function validatePassPhrase(value, account){
 
     return {invalid: invalid, error: error};
 }
-/*
-export function validatePhraseLength(value,status){
-  let invalid=status.invalid;
-  let error=status.error;
 
-  if (value !== '') {
-    invalid = value.length < 20;
-    error = invalid ? 'phrase looks too short.' : '';
-  } else {
-    // case if field is empty
-    invalid = false;
-    error = '';
-  }
-  return {invalid:invalid, error:error};
-}
-*/
-
-export function getAccountFromPhrase(value){
-    return {
-        account: secretPhraseToAccountId(value,false)
-    };
-}
-
+/**
+ * @param {string} value - user name
+ * @returns {object} - object with invalid and error properties
+ * @description - checks if the ethreum address is valid
+ */
 export function validateEthAddress(value){
     let invalid=false;
     let error="";
@@ -139,17 +66,13 @@ export function validateEthAddress(value){
     return {invalid: invalid, error: error};
 }
 
-/*
-function validateAddress(value){
-  const invalid = value.match('^ARDOR-[A-Z0-9_]{4}-[A-Z0-9_]{4}-[A-Z0-9_]{4}-[A-Z0-9_]{5}') ? false : true;
-  const error = invalid ? "this doesn't look like a valid ARDOR address." : '';
-  return({invalid: invalid, error: error});
-}
-*/
-
+/**
+ * @description - checks if the user has all the cards of the collection
+ * @param {array} userSet - list of user's assets
+ * @param {array} collectionSet - list of collection's assets
+ * @returns returns true if bs has all items of set as.
+ */
 function eqSet(userSet, collectionSet) {
-    //returns true if bs has all items of set as.
-    // eqSet(collection, user) to check if user has collection complete
     for (var assetId of collectionSet) {
         if (!userSet.has(assetId)) {
             return false;
@@ -158,9 +81,13 @@ function eqSet(userSet, collectionSet) {
     return true;
 }
 
+/**
+ * @param {array} userAssets - list of user's assets
+ * @param {array} collectionAssets - list of collection's assets
+ * @returns {object} - object with complete, userAssets, missingAssets and totalNum properties
+ * @description - check if every asset of the collection exists at least once in the users' assets
+ */
 export function validateWinner(userAssets, collectionAssets){
-    // function to check if every asset of the collection exists at least once in
-    // the users' assets
     const userValidAssets = [];
     userAssets.forEach(asset => {
         if (asset.quantityQNT > 0){
@@ -187,9 +114,15 @@ export function validateWinner(userAssets, collectionAssets){
     };
 }
 
-export function validatePin(value, status){
-    let invalid=status.invalid;
-    let error=status.error;
+/**
+ * @param {string} value - pin
+ * @param {object} status - object with invalid and error properties
+ * @returns {object} - object with invalid and error properties
+ * @description - checks if the pin is valid
+ */
+export function validatePin(value, status) {
+    let invalid = status.invalid;
+    let error = status.error;
     
     invalid = value.length < 4 ? true : false;
     error = invalid ? "too short" : '';
