4cf540292cb412ecb49ae4fca630d77fc4af9027
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-04 11:44:06 +0100

    Detele autoroutes
    Better info control

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import { Routes, Route } from 'react-router-dom';
 import { theme } from './themes/theme';
 import routes from './routes/routes';
 
+// General components
 import Header from './components/Navigation/Header/Header';
 import Footer from './components/Navigation/Footer/Footer';
+
+// Pages
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Restore from './pages/Restore/Restore';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <ChakraProvider theme={ theme }>
       <Header/>
       <Routes>
-        {
-          routes.map((route, index) => (
-            <Route
-              key = { index }
-              path = { route.path }
-              element = { <route.component/> }
-            />
-          ))
-        }
+        <Route path="/" element={ <Login/> } />
+        <Route path="/register" element={ <Register/> } />
+        <Route path="/restore" element={ <Restore/> } />
+        <Route path="/home" element={ <Home/> } />
       </Routes>
       <Footer/>
     </ChakraProvider>
