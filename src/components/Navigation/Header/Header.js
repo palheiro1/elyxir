9adc0fb155b78e22dc9971dc03eed278e531bcfd
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-04 11:39:48 +0100

    Ready for multi-header

--- src/components/Navigation/Header/Header.js
+++ src/components/Navigation/Header/Header.js
@@ -2,12 +2,18 @@ import Navigation from '../Navigation';
 
 /**
  * Footer component
+ * @param {boolean} isLogged - Indicates if the user is logged in or not
  * @returns {JSX.Element} Footer component
  * @dev This component is used to render the header navigation
  * @author Jesús Sánchez Fernández
  */
-const Header = () => {
-    return(<Navigation/>);
+const Header = ({ isLogged }) => {
+    return(
+        !isLogged ?
+            <Navigation/>
+            :
+            null
+    );
 }
 
 export default Header;
