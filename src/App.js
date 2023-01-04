f4814608308443603c5af60baa4a9bd8ff21cd95
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-04 11:52:27 +0100

    Implement infoAccount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
 
 import { theme } from './themes/theme';
-import routes from './routes/routes';
 
 // General components
 import Header from './components/Navigation/Header/Header';
@@ -14,16 +13,27 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Restore from './pages/Restore/Restore';
 import Home from './pages/Home/Home';
+import { cleanInfoAccount } from './data/DefaultInfo/cleanInfoAccount';
 
 function App() {
+
+  const [ infoAccount, setInfoAccount ] = useState(cleanInfoAccount);
+
   return (
-    <ChakraProvider theme={ theme }>
-      <Header/>
+    <ChakraProvider theme={ theme }> 
+      <Header isLogged = {infoAccount.token !== null && infoAccount.accountRs !== null} />
       <Routes>
-        <Route path="/" element={ <Login/> } />
+
+        <Route path="/" element={ <Login setInfoAccount = {setInfoAccount} /> } />
+
         <Route path="/register" element={ <Register/> } />
+
         <Route path="/restore" element={ <Restore/> } />
-        <Route path="/home" element={ <Home/> } />
+
+        <Route path="/home" element={ 
+          <Home infoAccount = {infoAccount} />
+        } />
+
       </Routes>
       <Footer/>
     </ChakraProvider>
---
+++ src/data/DefaultInfo/cleanInfoAccount.js
@@ -0,0 +1,10 @@
+export const cleanInfoAccount = () => {
+    return { 
+        name: null, 
+        accountRs: null,
+        usePin: null,
+        token: null,
+        timestamp: null,
+        backupDone: false
+    }
+}
