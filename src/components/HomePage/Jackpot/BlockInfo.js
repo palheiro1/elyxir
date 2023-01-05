d7b1d0509e71b3b504cdaa9604d465b88e810b00
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-05 15:12:46 +0100

    Components - Jackpot

---
+++ src/components/HomePage/Jackpot/BlockInfo.js
@@ -0,0 +1,47 @@
+import { Box, Table, Td, Tr } from "@chakra-ui/react"
+
+const BlockInfo = () => {
+    return (
+        <Box>
+            <Table variant="simple" m={4}>
+                <Tr>
+                    <Td py={1.5} borderBottom="0px">
+                        Remaining blocks
+                    </Td>
+                    <Td py={1.5} borderBottom="0px">
+                        8130
+                    </Td>
+                </Tr>
+
+                <Tr>
+                    <Td py={1.5} borderBottom="0px">
+                        Next block in
+                    </Td>
+                    <Td py={1.5} borderBottom="0px">
+                        -7 sec
+                    </Td>
+                </Tr>
+
+                <Tr>
+                    <Td py={1.5} borderBottom="0px">
+                        Jackpot block
+                    </Td>
+                    <Td py={1.5} borderBottom="0px">
+                        1567899
+                    </Td>
+                </Tr>
+
+                <Tr>
+                    <Td py={1.5} borderBottom="0px">
+                        Current block
+                    </Td>
+                    <Td py={1.5} borderBottom="0px">
+                        1665150
+                    </Td>
+                </Tr>
+            </Table>
+        </Box>
+    )
+}
+
+export default BlockInfo
---
+++ src/components/HomePage/Jackpot/Countdown.js
@@ -0,0 +1,39 @@
+import { Box, HStack, IconButton, Text, VStack } from "@chakra-ui/react"
+import { GiCutDiamond } from "react-icons/gi"
+
+const Countdown = () => {
+    return (
+        <Box>
+            <HStack>
+                <IconButton icon={<GiCutDiamond />} size="lg" m={4} mr={2} p={4} fontSize="2xl" bg="whiteAlpha.100" />
+                <VStack align="flex-start">
+                    <Text fontSize="2xl" fontWeight="bold" mb={-3}>3581.23 IGNIS</Text>
+                    <Text fontSize="sm">(215.60 USD)</Text>
+                </VStack>
+            </HStack>
+            <HStack spacing={2} ml={4}>
+                <Box p={2} bg="#121D31" rounded="lg" minW="60px">
+                    <Text textAlign="center" fontSize="lg" fontWeight="bold">5</Text>
+                    <Text textAlign="center" fontSize="xs">days</Text>
+                </Box>
+
+                <Box p={2} bg="#121D31" rounded="lg" minW="60px">
+                    <Text textAlign="center" fontSize="lg" fontWeight="bold">15</Text>
+                    <Text textAlign="center" fontSize="xs">hours</Text>
+                </Box>
+
+                <Box p={2} bg="#121D31" rounded="lg" minW="60px">
+                    <Text textAlign="center" fontSize="lg" fontWeight="bold">24</Text>
+                    <Text textAlign="center" fontSize="xs">minutes</Text>
+                </Box>
+
+                <Box p={2} bg="#121D31" rounded="lg" minW="60px">
+                    <Text textAlign="center" fontSize="lg" fontWeight="bold">5</Text>
+                    <Text textAlign="center" fontSize="xs">seconds</Text>
+                </Box>
+            </HStack>
+        </Box>
+    )
+}
+
+export default Countdown
