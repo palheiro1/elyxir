c66997763351775ba49ccc1728a4ea06103e9d87
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-09 22:54:25 +0100

    Rework LateralMenu

--- src/components/LateralMenu/LateralMenu.js
+++ src/components/LateralMenu/LateralMenu.js
@@ -1,68 +1,117 @@
-import { Box, Button, HStack, Switch, Text, VStack } from "@chakra-ui/react"
+import {
+    Box,
+    Button,
+    Stack,
+    Switch,
+    Text,
+    VStack,
+} from '@chakra-ui/react';
 
-import { BsReverseLayoutTextWindowReverse, BsClockHistory } from "react-icons/bs";
-import { GiCardRandom, GiCutDiamond } from "react-icons/gi";
-import { AiOutlineSetting, AiOutlineShoppingCart } from "react-icons/ai";
-import { BiPackage } from "react-icons/bi";
+import {
+    BsReverseLayoutTextWindowReverse,
+    BsClockHistory,
+} from 'react-icons/bs';
+
+import { GiCardRandom, GiCutDiamond } from 'react-icons/gi';
+import { AiOutlineSetting, AiOutlineShoppingCart } from 'react-icons/ai';
+import { BiPackage } from 'react-icons/bi';
 
 const LateralMenu = ({ children }) => {
-
     const isActive = true;
-    const bgColor = isActive && "white";
-    const textColor = isActive ? "black" : "white";
-    const iconColor = isActive ? "blue" : "white";
+    const bgColor = isActive && 'white';
+    const textColor = isActive ? 'black' : 'white';
+    const iconColor = isActive ? 'blue' : 'white';
 
     return (
-        <HStack>
-            <Box>
-                <HStack>
-                    <Text fontSize="xl" ml={2} mr={4}>
+        <Stack direction="row">
+            <Box mr={8}>
+                <Stack direction="column">
+                    <Text
+                        fontSize="xl"
+                        ml={2}
+                        mr={4}
+                        mb={4}
+                        textAlign="center"
+                        fontWeight="bold"
+                    >
                         My Wallet
                     </Text>
 
-                    <Box>
-                        <HStack p={2}>
-                            <Text>Show all cards</Text>
-                            <Switch size="md" p={4} colorScheme="blue" />
-                        </HStack>
-                    </Box>
-                </HStack>
-                <VStack align="flex-start" ml={2}>
-
-                    <Button minW="150px" minH="50px" leftIcon={<BsReverseLayoutTextWindowReverse size={17.5} color={iconColor} />} bgColor={bgColor} textColor={textColor}>
+                    <Box></Box>
+                </Stack>
+                <VStack align="flex-start">
+                    <Button
+                        minW="150px"
+                        minH="50px"
+                        leftIcon={
+                            <BsReverseLayoutTextWindowReverse
+                                size={17.5}
+                                color={iconColor}
+                            />
+                        }
+                        bgColor={bgColor}
+                        textColor={textColor}
+                    >
                         Overview
                     </Button>
 
-                    <Button minW="150px" minH="50px" leftIcon={<GiCardRandom />}>
+                    <Button
+                        minW="150px"
+                        minH="50px"
+                        leftIcon={<GiCardRandom />}
+                    >
                         Inventory
                     </Button>
 
-                    <Button minW="150px" minH="50px" leftIcon={<BsClockHistory />} >
+                    <Button
+                        minW="150px"
+                        minH="50px"
+                        leftIcon={<BsClockHistory />}
+                    >
                         History
                     </Button>
 
-                    <Button minW="150px" minH="50px" leftIcon={<AiOutlineShoppingCart />} >
+                    <Button
+                        minW="150px"
+                        minH="50px"
+                        leftIcon={<AiOutlineShoppingCart />}
+                    >
                         Market
                     </Button>
 
-                    <Button minW="150px" minH="50px" leftIcon={<GiCutDiamond />}>
+                    <Button
+                        minW="150px"
+                        minH="50px"
+                        leftIcon={<GiCutDiamond />}
+                    >
                         Jackpot
                     </Button>
 
-                    <Button minW="150px" minH="50px" leftIcon={<AiOutlineSetting />}>
+                    <Button
+                        minW="150px"
+                        minH="50px"
+                        leftIcon={<AiOutlineSetting />}
+                    >
                         Account
                     </Button>
 
-                    <Button minW="150px" minH="50px" bgColor="orange.600" leftIcon={<BiPackage />}>
+                    <Button
+                        minW="150px"
+                        minH="50px"
+                        bgColor="orange.600"
+                        leftIcon={<BiPackage />}
+                    >
                         Buy pack
                     </Button>
                 </VStack>
+                <Stack p={2} align="center" pt={8} >
+                    <Text fontWeight="bold">Show all cards</Text>
+                    <Switch size="md" colorScheme="blue" />
+                </Stack>
             </Box>
-            <Box>
-                {children}
-            </Box>
-        </HStack>
-    )
-}
+            <Box width="100%">{children}</Box>
+        </Stack>
+    );
+};
 
-export default LateralMenu
+export default LateralMenu;
