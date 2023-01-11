15d6ceaa33f558169defb9a990f0619492b00070
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-11 23:30:27 +0100

    Buttons border

--- src/components/MarketPage/Market.js
+++ src/components/MarketPage/Market.js
@@ -19,11 +19,11 @@ const Market = ({ infoAccount, cards }) => {
                 <SortAndFilterMenu cards={cards} setCardsFiltered={setCardsFiltered} />
             </Box>
             <Box w="100%">
-                <Button isActive={option===0} w="50%" size="lg" rounded="node" fontWeight="medium" fontSize="md" onClick={() => setOption(0)} >
+                <Button isActive={option===0} w="50%" size="lg" rounded="node" fontWeight="medium" fontSize="md" onClick={() => setOption(0)} borderLeftRadius="lg" >
                     Market
                 </Button>
 
-                <Button isActive={option===1} w="50%" size="lg" rounded="node" fontWeight="medium" fontSize="md" onClick={() => setOption(1)}>
+                <Button isActive={option===1} w="50%" size="lg" rounded="node" fontWeight="medium" fontSize="md" onClick={() => setOption(1)} borderRightRadius="lg">
                     Trades and orders
                 </Button>
             </Box>
