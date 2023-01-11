5a9b972f0b6c73712dcc832651e9da6b14c39ce7
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-11 23:09:47 +0100

    Beautiful InOutTx

--- src/components/Tables/InOutTransaction.js
+++ src/components/Tables/InOutTransaction.js
@@ -1,15 +1,19 @@
-import { ArrowUpIcon, ArrowDownIcon } from "@chakra-ui/icons"
-import { Stack, Text } from "@chakra-ui/react"
+import { ArrowUpIcon, ArrowDownIcon } from '@chakra-ui/icons';
+import { Stack, Text } from '@chakra-ui/react';
 
 const InOutTransaction = ({ type }) => {
-    const color = type === "in" ? "green" : "blue.600"
-    const msg = type === "in" ? "Received" : "Sent"
-    return(
+    const color = type === 'in' ? 'green' : 'blue.600';
+    const msg = type === 'in' ? 'Received' : 'Sent';
+    return (
         <Stack direction="row">
             <Text color={color}> {msg} </Text>
-            {type === "in" ? <ArrowDownIcon fontSize="xl" fontWeight="bold" color={color} /> : <ArrowUpIcon fontSize="xl" color={color} />}
+            {type === 'in' ? (
+                <ArrowDownIcon fontSize="xl" fontWeight="bold" color={color} />
+            ) : (
+                <ArrowUpIcon fontSize="xl" color={color} />
+            )}
         </Stack>
-    )
-}
+    );
+};
 
-export default InOutTransaction
+export default InOutTransaction;
