6178bfa2e7acfeed9f96f5d21076cdcf97152835
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-12 19:06:48 +0100

    Update Account.js

--- src/components/Pages/AccountPage/Account.js
+++ src/components/Pages/AccountPage/Account.js
@@ -11,42 +11,17 @@ import {
     Text,
     useColorModeValue,
 } from '@chakra-ui/react';
+import UserDataItem from './UserDataItem';
 
 const Account = ({ infoAccount }) => {
     console.log("🚀 ~ file: Account.js:16 ~ Account ~ infoAccount", infoAccount)
 
+    const { accountRs, name, IGNISBalance, GIFTZBalance } = infoAccount;
     const bgColor = useColorModeValue("blackAlpha.100", "whiteAlpha.100")
 
     return (
         <Grid templateColumns="repeat(3, 1fr)" gap={4}>
-            <GridItem>
-                <Box p={6} bgColor={bgColor} rounded="lg" mb={2}>
-                    <Heading fontSize="lg" pb={2}>
-                        Your Ardor account
-                    </Heading>
-                    <Text>{infoAccount.accountRs}</Text>
-                </Box>
-                <Box p={6} bgColor={bgColor} rounded="lg" mb={2}>
-                    <Heading fontSize="lg" pb={2}>
-                        User
-                    </Heading>
-                    <Text>{infoAccount.name}</Text>
-                </Box>
-                <Grid templateColumns="repeat(2, 1fr)" p={6} bgColor={bgColor} rounded="lg">
-                    <GridItem>
-                        <Heading fontSize="lg" pb={2}>
-                            IGNIS
-                        </Heading>
-                        <Text fontSize="sm">21.8 (1.52 USD)</Text>
-                    </GridItem>
-                    <GridItem>
-                        <Heading fontSize="lg" pb={2}>
-                            GIFTZ
-                        </Heading>
-                        <Text fontSize="sm">3</Text>
-                    </GridItem>
-                </Grid>
-            </GridItem>
+            <UserDataItem name={name} accountRs={accountRs} IGNISBalance={IGNISBalance} GIFTZBalance={GIFTZBalance} bgColor={bgColor} />
             <GridItem>
                 <Box p={4} bgColor={bgColor} rounded="lg" mb={2}>
                     <Heading fontSize="lg" pb={2}>
