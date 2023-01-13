dfefd980c8857eaf95fba545d11b9dffeae16a66
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-13 02:02:30 +0100

    Update InOutTransaction.js

--- src/components/Tables/InOutTransaction.js
+++ src/components/Tables/InOutTransaction.js
@@ -1,16 +1,29 @@
-import { ArrowUpIcon, ArrowDownIcon } from '@chakra-ui/icons';
+import { ArrowUpIcon, ArrowDownIcon, ArrowUpDownIcon } from '@chakra-ui/icons';
 import { Stack, Text } from '@chakra-ui/react';
 
 const InOutTransaction = ({ type }) => {
     const color = type === 'in' ? 'green' : 'blue.600';
-    const msg = type === 'in' ? 'Received' : 'Sent';
+    
+    let msg = '';
+    if(type === 'in') {
+        msg = 'Received';
+    } else if(type === 'out') {
+        msg = 'Sent';
+    } else if(type === 'placed') {
+        msg = 'Placed';
+    }
+
     return (
         <Stack direction="row">
             <Text color={color}> {msg} </Text>
-            {type === 'in' ? (
+            {type === 'in' && (
                 <ArrowDownIcon fontSize="xl" fontWeight="bold" color={color} />
-            ) : (
+            )}
+            {type === 'out' && (
                 <ArrowUpIcon fontSize="xl" color={color} />
+            )}
+            {type === 'placed' && (
+                <ArrowUpDownIcon fontSize="xl" color={color} />
             )}
         </Stack>
     );
