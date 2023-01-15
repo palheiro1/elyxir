f47806deb4b5d79fc2fa38a6e80cdcdcf87316be
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-15 23:16:55 +0100

    Added card version

--- src/components/Pages/MarketPage/TradesAndOrders/TradesOrOrderItem.js
+++ src/components/Pages/MarketPage/TradesAndOrders/TradesOrOrderItem.js
@@ -2,12 +2,38 @@ import { Stack, Td, Text, Tr } from "@chakra-ui/react";
 import InOutTxMarket from "../InOutTxMarket";
 
 import { FaCoins } from "react-icons/fa";
+import TableCard from "../../../Cards/TableCard";
 
-const TradesOrOrderItem = ({ type, name, amount, price, date, sellerOrBuyer }) => {
+const TradesOrOrderItem = ({ type, name, amount, price, date, sellerOrBuyer, card }) => {
+    console.log("🚀 ~ file: TradesOrOrderItem.js:8 ~ TradesOrOrderItem ~ card", card)
+    if(card === "GEM" || card === undefined) {
+        return (
+            <Tr>
+                <Td><InOutTxMarket type={type} /></Td>
+                <Td textAlign="center" fontWeight="bold">{name}</Td>
+                <Td>{amount}</Td>
+                <Td>
+                    <Stack direction="row" align="center">
+                        <FaCoins color="yellow" />
+                        <Text>{price}</Text>
+                    </Stack>
+                </Td>
+                <Td>{date}</Td>
+                <Td>{sellerOrBuyer}</Td>
+            </Tr>
+        );
+    }
     return (
         <Tr>
             <Td><InOutTxMarket type={type} /></Td>
-            <Td>{name}</Td>
+            <Td>
+                <TableCard
+                    image={card.cardImgUrl}
+                    title={card.name}
+                    continent={card.channel}
+                    rarity={card.rarity}
+                />
+            </Td>
             <Td>{amount}</Td>
             <Td>
                 <Stack direction="row" align="center">
