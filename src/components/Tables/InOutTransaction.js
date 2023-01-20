0a16f1b60afbb86fed342b61fee61e2081d432d6
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-20 15:51:40 +0100

    Refactor

--- src/components/Tables/InOutTransaction.js
+++ src/components/Tables/InOutTransaction.js
@@ -1,32 +1,45 @@
 import { ArrowUpIcon, ArrowDownIcon, ArrowUpDownIcon } from '@chakra-ui/icons';
 import { Stack, Text } from '@chakra-ui/react';
 
-const InOutTransaction = ({ type }) => {
-    const color = type === 'in' ? 'green' : 'blue.600';
-    
-    let msg = '';
-    if(type === 'in') {
-        msg = 'Received';
-    } else if(type === 'out') {
-        msg = 'Sent';
-    } else if(type === 'placed') {
-        msg = 'Placed';
-    }
+const InOutTransaction = ({ type, style = 2 }) => {
+    if (style === 1) {
+        const color = type === 'in' ? 'green' : 'blue.600';
 
-    return (
-        <Stack direction="row">
-            <Text color={color}> {msg} </Text>
-            {type === 'in' && (
-                <ArrowDownIcon fontSize="xl" fontWeight="bold" color={color} />
-            )}
-            {type === 'out' && (
-                <ArrowUpIcon fontSize="xl" color={color} />
-            )}
-            {type === 'placed' && (
-                <ArrowUpDownIcon fontSize="xl" color={color} />
-            )}
-        </Stack>
-    );
+        let msg = '';
+        if (type === 'in') {
+            msg = 'Received';
+        } else if (type === 'out') {
+            msg = 'Sent';
+        } else if (type === 'placed') {
+            msg = 'Placed';
+        }
+
+        return (
+            <Stack direction="row">
+                <Text color={color}> {msg} </Text>
+                {type === 'in' && <ArrowDownIcon fontSize="xl" fontWeight="bold" color={color} />}
+                {type === 'out' && <ArrowUpIcon fontSize="xl" color={color} />}
+                {type === 'placed' && <ArrowUpDownIcon fontSize="xl" color={color} />}
+            </Stack>
+        );
+    } else {
+        const typeTx = type.toLowerCase();
+        let color;
+
+        let icon;
+        if (typeTx === 'in') {
+            color = 'green';
+            icon = <ArrowDownIcon fontSize="xl" fontWeight="bold" bgColor={color} rounded="full" />;
+        } else if (typeTx === 'out') {
+            color = 'blue.600';
+            icon = <ArrowUpIcon fontSize="xl" fontWeight="bold" bgColor={color} rounded="full" />;
+        } else if (typeTx === 'placed') {
+            color = 'orange.600';
+            icon = <ArrowUpDownIcon fontSize="xl" fontWeight="bold" bgColor={color} rounded="full" />;
+        }
+
+        return icon;
+    }
 };
 
 export default InOutTransaction;
