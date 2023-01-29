87c9de6f40d54366cb54116825f3edf9c3811eab
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-29 20:31:05 +0100

    Update Bridge.js

--- src/components/Pages/Bridge/Bridge.js
+++ src/components/Pages/Bridge/Bridge.js
@@ -1,9 +1,17 @@
-const Bridge = () => {
-    return(
-        <div>
-            <h1>Bridge</h1>
-        </div>
-    )
-}
+import { useState } from 'react';
+import SectionSwitch from './SectionSwitch';
+import SwapToPolygon from './SwapToPolygon';
+
+const Bridge = ({ infoAccount, cards }) => {
+    const [option, setOption] = useState(0);
+
+    return (
+        <>
+            <SectionSwitch option={option} setOption={setOption} />
+
+            { option === 0 && <SwapToPolygon infoAccount={infoAccount} cards={cards} /> }
+        </>
+    );
+};
 
 export default Bridge;
