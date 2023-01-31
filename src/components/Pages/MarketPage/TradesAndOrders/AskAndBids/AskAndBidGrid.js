26690bb1414d6767f86a3a69fb4ffcf41d3b4657
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-31 13:45:19 +0100

    Refactor Orders

--- src/components/Pages/MarketPage/TradesAndOrders/AskAndBids/AskAndBidGrid.js
+++ src/components/Pages/MarketPage/TradesAndOrders/AskAndBids/AskAndBidGrid.js
@@ -1,21 +1,7 @@
-import {
-    SimpleGrid,
-    Table,
-    TableContainer,
-    Tbody,
-    Td,
-    Text,
-    Th,
-    Thead,
-    Tr,
-    useColorModeValue,
-    useDisclosure,
-} from '@chakra-ui/react';
+import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
 import { useRef, useState } from 'react';
-import { NQTDIVIDER } from '../../../../../data/CONSTANTS';
-import { getAsset } from '../../../../../utils/cardsUtils';
 import CancelDialog from '../../../../Modals/TradeDialog/CancelDialog/CancelDialog';
-import AskOrBidItem from './AskOrBidItem';
+import AskBidTable from './AskAndBidTable';
 
 /**
  * @name AskAndBidGrid
@@ -34,91 +20,27 @@ const AskAndBidGrid = ({ cards, askOrders, bidOrders, onlyOneAsset = false, user
     const { isOpen, onOpen, onClose } = useDisclosure();
     const refCancel = useRef();
 
-    const bgTitleColor = useColorModeValue('blackAlpha.100', 'whiteAlpha.100');
-    const bgHeadColor = useColorModeValue('blackAlpha.300', 'whiteAlpha.300');
-    const borderColor = useColorModeValue('blackAlpha.100', 'whiteAlpha.100');
-
     return (
         <>
             <SimpleGrid columns={{ base: 1, md: 2 }} mt={2} shadow="lg">
-                <TableContainer
-                    mt={4}
-                    border="2px"
-                    borderColor={borderColor}
-                    borderLeftRadius="lg"
-                    shadow="inner"
-                    boxShadow="md">
-                    <Text textAlign="center" p={4} fontSize="lg" borderBottom="1px" bgColor={bgTitleColor}>
-                        Asks
-                    </Text>
-
-                    <Table variant="simple">
-                        <Thead backgroundColor={bgHeadColor}>
-                            <Tr>
-                                {!onlyOneAsset && <Th textAlign="center">Asset</Th>}
-                                <Th textAlign="center">Ignis</Th>
-                                <Th textAlign="center">Amount</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {askOrders.map(order => {
-                                const _asset = onlyOneAsset ? order.asset : getAsset(order.asset, cards);
-                                return (
-                                    <AskOrBidItem
-                                        key={order.orderFullHash}
-                                        order={order.order}
-                                        asset={_asset}
-                                        ignis={order.priceNQTPerShare / NQTDIVIDER}
-                                        amount={order.quantityQNT}
-                                        isAsk={true}
-                                        onOpen={onOpen}
-                                        setSelectedOrder={setSelectedOrder}
-                                        onlyOneAsset={onlyOneAsset}
-                                        canDelete={canDelete}
-                                    />
-                                );
-                            })}
-                        </Tbody>
-                    </Table>
-                </TableContainer>
-                <TableContainer
-                    mt={4}
-                    border="2px"
-                    borderColor={borderColor}
-                    borderRightRadius="lg"
-                    shadow="inner"
-                    boxShadow="md">
-                    <Text textAlign="center" p={4} fontSize="lg" borderBottom="1px" bgColor={bgTitleColor}>
-                        Bids
-                    </Text>
-                    <Table variant="simple">
-                        <Thead backgroundColor={bgHeadColor}>
-                            <Tr>
-                                {!onlyOneAsset && <Th textAlign="center">Asset</Th>}
-                                <Th textAlign="center">Ignis</Th>
-                                <Th textAlign="center">Amount</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {bidOrders.map(order => {
-                                const _asset = onlyOneAsset ? order.asset : getAsset(order.asset, cards);
-                                return (
-                                    <AskOrBidItem
-                                        key={order.orderFullHash}
-                                        order={order.order}
-                                        asset={_asset}
-                                        ignis={Number(order.priceNQTPerShare / NQTDIVIDER)}
-                                        amount={order.quantityQNT}
-                                        onOpen={onOpen}
-                                        setSelectedOrder={setSelectedOrder}
-                                        onlyOneAsset={onlyOneAsset}
-                                        canDelete={canDelete}
-                                    />
-                                );
-                            })}
-                        </Tbody>
-                    </Table>
-                </TableContainer>
+                <AskBidTable
+                    type={'Asks'}
+                    onlyOneAsset={onlyOneAsset}
+                    orders={askOrders}
+                    cards={cards}
+                    onOpen={onOpen}
+                    setSelectedOrder={setSelectedOrder}
+                    canDelete={canDelete}
+                />
+                <AskBidTable
+                    type={'Bids'}
+                    onlyOneAsset={onlyOneAsset}
+                    orders={bidOrders}
+                    cards={cards}
+                    onOpen={onOpen}
+                    setSelectedOrder={setSelectedOrder}
+                    canDelete={canDelete}
+                />
             </SimpleGrid>
             <CancelDialog
                 reference={refCancel}
---
+++ src/components/Pages/MarketPage/TradesAndOrders/AskAndBids/AskAndBidTable.js
@@ -0,0 +1,54 @@
+import { Table, TableContainer, Tbody, Text, Th, Thead, Tr, useColorModeValue } from '@chakra-ui/react';
+import { NQTDIVIDER } from '../../../../../data/CONSTANTS';
+import { getAsset } from '../../../../../utils/cardsUtils';
+import AskOrBidItem from './AskOrBidItem';
+
+const AskBidTable = ({ type, onlyOneAsset, orders, cards, onOpen, setSelectedOrder, canDelete }) => {
+    const bgTitleColor = useColorModeValue('blackAlpha.100', 'whiteAlpha.100');
+    const bgHeadColor = useColorModeValue('blackAlpha.300', 'whiteAlpha.300');
+    const borderColor = useColorModeValue('blackAlpha.100', 'whiteAlpha.100');
+    return (
+        <TableContainer
+            mt={4}
+            border="2px"
+            borderColor={borderColor}
+            borderLeftRadius="lg"
+            shadow="inner"
+            boxShadow="md">
+            <Text textAlign="center" p={4} fontSize="lg" borderBottom="1px" bgColor={bgTitleColor}>
+                {type}
+            </Text>
+
+            <Table variant="simple">
+                <Thead backgroundColor={bgHeadColor}>
+                    <Tr>
+                        {!onlyOneAsset && <Th textAlign="center">Asset</Th>}
+                        <Th textAlign="center">Ignis</Th>
+                        <Th textAlign="center">Amount</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {orders.map(order => {
+                        const _asset = onlyOneAsset ? order.asset : getAsset(order.asset, cards);
+                        return (
+                            <AskOrBidItem
+                                key={order.orderFullHash}
+                                order={order.order}
+                                asset={_asset}
+                                ignis={order.priceNQTPerShare / NQTDIVIDER}
+                                amount={order.quantityQNT}
+                                isAsk={true}
+                                onOpen={onOpen}
+                                setSelectedOrder={setSelectedOrder}
+                                onlyOneAsset={onlyOneAsset}
+                                canDelete={canDelete}
+                            />
+                        );
+                    })}
+                </Tbody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default AskBidTable;
