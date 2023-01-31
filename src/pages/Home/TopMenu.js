5bc35f135193bb0eee3324459f888a07a4d128f2
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-01-31 14:23:13 +0100

    Account tooltip + easy copy

--- src/pages/Home/TopMenu.js
+++ src/pages/Home/TopMenu.js
@@ -1,7 +1,16 @@
-import { Flex, Spacer, Stack, Text } from '@chakra-ui/react';
+import { Flex, Spacer, Stack, Text, Tooltip, useToast } from '@chakra-ui/react';
 import CurrencyMenu from '../../components/CurrencyMenu/CurrencyMenu';
+import { copyToast } from '../../utils/alerts';
 
 const TopMenu = ({ infoAccount, goToSection }) => {
+
+    const toast = useToast();
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(infoAccount.accountRs);
+        copyToast("ARDOR Account", toast)
+    };
+
     return (
         <>
             <Flex w="100%">
@@ -9,9 +18,11 @@ const TopMenu = ({ infoAccount, goToSection }) => {
                     <Text fontSize="sm" fontWeight="bold" mb={-3}>
                         {infoAccount.name}
                     </Text>
-                    <Text fontSize="md" fontWeight="bold">
-                        {infoAccount.accountRs}
-                    </Text>
+                    <Tooltip label="Click to copy" hasArrow placement='top-end'>
+                        <Text fontSize="md" fontWeight="bold" onClick={copyToClipboard} _hover={{ cursor: "pointer"}}>
+                            {infoAccount.accountRs}
+                        </Text>
+                    </Tooltip>
                 </Stack>
                 <Spacer />
                 <CurrencyMenu infoAccount={infoAccount} goToSection={goToSection} />
