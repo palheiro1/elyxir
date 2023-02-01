269f4ad320f54d5f66d0394d52f17b57b8636071
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-02-01 13:57:51 +0100

    Update validators.js

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -29,75 +29,11 @@ export function checkIsValidPassphrase(passphrase, expectedAccount) {
     return actualAccount === expectedAccount;
 }
 
-
+/**
+ * @param {string} account - account address
+ * @returns {Boolean} - true if the account is valid, false otherwise
+ * @description - checks if the pass phrase belongs to the account
+ */
 export const isArdorAccount = account => {
     return /^ARDOR-[A-Z0-9_]{4}-[A-Z0-9_]{4}-[A-Z0-9_]{4}-[A-Z0-9_]{5}/.test(account);
 };
-
-/**
- * @description - checks if the user has all the cards of the collection
- * @param {array} userSet - list of user's assets
- * @param {array} collectionSet - list of collection's assets
- * @returns returns true if bs has all items of set as.
- */
-function eqSet(userSet, collectionSet) {
-    for (var assetId of collectionSet) {
-        if (!userSet.has(assetId)) {
-            return false;
-        }
-    }
-    return true;
-}
-
-/**
- * @param {array} userAssets - list of user's assets
- * @param {array} collectionAssets - list of collection's assets
- * @returns {object} - object with complete, userAssets, missingAssets and totalNum properties
- * @description - check if every asset of the collection exists at least once in the users' assets
- */
-export function validateWinner(userAssets, collectionAssets) {
-    const userValidAssets = [];
-    userAssets.forEach(asset => {
-        if (asset.quantityQNT > 0) {
-            userValidAssets.push(asset.asset);
-        }
-    });
-
-    const userSet = new Set(userValidAssets);
-
-    const collectionSet = new Set(
-        collectionAssets.map(asset => {
-            return asset.asset;
-        })
-    );
-    const intersection = new Set([...userSet].filter(x => collectionSet.has(x)));
-
-    const sets_equal = eqSet(intersection, collectionSet);
-
-    // difference between intersection (all user's cards) and collection
-    let difference = new Set([...collectionSet].filter(x => !intersection.has(x)));
-    // the difference are the missing cards.
-
-    return {
-        complete: sets_equal,
-        userAssets: [...intersection],
-        missingAssets: [...difference],
-        totalNum: collectionAssets.length,
-    };
-}
-
-/**
- * @param {string} value - pin
- * @param {object} status - object with invalid and error properties
- * @returns {object} - object with invalid and error properties
- * @description - checks if the pin is valid
- */
-export function validatePin(value, status) {
-    let invalid = status.invalid;
-    let error = status.error;
-
-    invalid = value.length < 4 ? true : false;
-    error = invalid ? 'too short' : '';
-
-    return { invalid: invalid, error: error };
-}
