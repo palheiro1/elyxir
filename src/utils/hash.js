004b18e2fb39258b2e9aeb473e851f1f89d300a3
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-02-01 13:54:23 +0100

    Update hash.js

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -3,3 +3,11 @@ import Crypto from 'crypto-browserify';
 export const generateHash = (text) => {
     return Crypto.createHash('sha256').update(JSON.stringify(text)).digest('hex');
 }
+
+export const compareHash = (text, hash) => {
+    return generateHash(text) === hash;
+}
+
+export const isHash = (hash) => {
+    return /^[0-9a-fA-F]{64}$/.test(hash);
+}
