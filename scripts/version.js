488405713a4152b20132c28ee87bee17eb679211
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-02-09 18:47:10 +0100

    Update version.js

--- scripts/version.js
+++ scripts/version.js
@@ -1,10 +1,10 @@
-const childProcess = require("child_process");
-const fs = require("fs");
-const readline = require("readline");
+const childProcess = require('child_process');
+const fs = require('fs');
+const readline = require('readline');
 
 async function processLineByLine() {
-    const fileStream = fs.createReadStream(".env");
-    
+    const fileStream = fs.createReadStream('.env');
+
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity,
@@ -17,32 +17,35 @@ async function processLineByLine() {
     return outputs;
 }
 
-function writeToEnv(key = "", value = "") {
-    const empty = key === "" && value === "";
+function writeToEnv(key = '', value = '') {
+    const empty = key === '' && value === '';
     if (empty) {
-        fs.writeFile(".env", "", () => {});
+        fs.writeFile('.env', '', () => {});
     } else {
-        fs.appendFile(".env", `${key}='${value.trim()}'\n`, (err) => {
+        fs.appendFile('.env', `${key}='${value.trim()}'\n`, err => {
             if (err) console.log(err);
         });
     }
-}(async () => {
-    const results = await processLineByLine();writeToEnv();for (const result of results) {
-        if (!result.includes("REACT_APP_GIT_BRANCH")) {
-            if (!result.includes("REACT_APP_GIT_SHA")) {
-                const lineArray = result.split("=");
+}
+(async () => {
+    const results = await processLineByLine();
+    writeToEnv();
+    for (const result of results) {
+        if (!result.includes('REACT_APP_GIT_BRANCH')) {
+            if (!result.includes('REACT_APP_GIT_SHA')) {
+                const lineArray = result.split('=');
                 if (lineArray.length === 2) {
-                    const key = lineArray[0].replace(/'/gi, "");
-                    const value = lineArray[1].replace(/'/gi, "");
+                    const key = lineArray[0].replace(/'/gi, '');
+                    const value = lineArray[1].replace(/'/gi, '');
                     writeToEnv(key, value);
                 }
             }
         }
     }
-    childProcess.exec("git rev-parse --abbrev-ref HEAD", (err, stdout) => {
-        writeToEnv("REACT_APP_GIT_BRANCH", stdout);
+    childProcess.exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
+        writeToEnv('REACT_APP_GIT_BRANCH', stdout);
     });
-    childProcess.exec("git rev-parse --short HEAD", (err, stdout) => {
-        writeToEnv("REACT_APP_GIT_SHA", stdout);
+    childProcess.exec('git rev-parse --short HEAD', (err, stdout) => {
+        writeToEnv('REACT_APP_GIT_SHA', stdout);
     });
 })();
