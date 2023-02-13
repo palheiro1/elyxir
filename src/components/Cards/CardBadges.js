f3531b37c6342348c600dc6e244726168c59346d
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-02-13 19:24:59 +0100

    Badges fix

--- src/components/Cards/CardBadges.js
+++ src/components/Cards/CardBadges.js
@@ -1,13 +1,14 @@
-import { Stack, Text, useColorModeValue } from "@chakra-ui/react";
-import { RARITY_COLORS } from "../../data/CONSTANTS";
+import { Stack, Text, useColorModeValue } from '@chakra-ui/react';
+import { RARITY_COLORS } from '../../data/CONSTANTS';
 
-const CardBadges = ({ rarity, continent, size = "md" }) => {
-
+const CardBadges = ({ rarity, continent, size = 'md' }) => {
     const badgeColor = useColorModeValue('blackAlpha.600', 'whiteAlpha.300');
 
     return (
-        <Stack direction={{ base: 'column', md: 'row' }} spacing={1} align="center">
+        <Stack direction={{ base: 'column', md: 'row' }} spacing={1}>
             <Text
+                w={{ base: '7.5rem', md: 'auto' }}
+                h={{ base: '1rem', md: 'auto' }}
                 textAlign="center"
                 fontSize={size}
                 bgGradient={RARITY_COLORS[rarity]}
@@ -16,7 +17,15 @@ const CardBadges = ({ rarity, continent, size = "md" }) => {
                 px={2}>
                 {rarity}
             </Text>
-            <Text fontSize={size} bgColor={badgeColor} rounded="lg" color="white" px={2} textAlign="center">
+            <Text
+                fontSize={size}
+                w={{ base: '7.5rem', md: 'auto' }}
+                h={{ base: '1rem', md: 'auto' }}
+                bgColor={badgeColor}
+                rounded="lg"
+                color="white"
+                px={2}
+                textAlign="center">
                 {continent}
             </Text>
         </Stack>
