2176e66f3457eefb2f90a25280b7881b7a1ddc19
JSanchezFDZ <jsanchezfdz95@gmail.com>
2023-02-21 23:21:46 +0100

    Refactor Messages

--- src/components/Pages/ChatPage/Messages.js
+++ src/components/Pages/ChatPage/Messages.js
@@ -1,4 +1,5 @@
-import { Box, Button, Center, SimpleGrid, Stack, Text } from '@chakra-ui/react';
+import { Box, Stack, Text } from '@chakra-ui/react';
+import OneMessage from './OneMessage';
 
 const Messages = ({ messages = [] }) => {
     return (
@@ -7,41 +8,8 @@ const Messages = ({ messages = [] }) => {
                 Total unread: <b>{messages.length}</b>
             </Text>
             <Stack w="100%" rounded="md" p={4} my={4} shadow="md" bgColor="whiteAlpha.50">
-                <Box bgColor="whiteAlpha.200" rounded="lg" py={2}>
-                    <SimpleGrid columns={3} spacing={4}>
-                        <Center>
-                            <Text align="center">ARDOR-5NCL-DRBZ-XBWF-DDN5T</Text>
-                        </Center>
-                        <Center>
-                            <Text align="center">Last message 12 minutes ago</Text>
-                        </Center>
-                        <Button variant="outline" mx={12}>Click to decrypt conversation</Button>
-                    </SimpleGrid>
-                </Box>
-                <Box bgColor="whiteAlpha.200" rounded="lg" py={2}>
-                    <SimpleGrid columns={3} spacing={4}>
-                        <Center>
-                            <Text align="center">ARDOR-5NCL-DRBZ-XBWF-DDN5T</Text>
-                        </Center>
-                        <Center>
-                            <Text align="center">Last message 12 minutes ago</Text>
-                        </Center>
-                        <Button variant="outline" mx={12}>Click to decrypt conversation</Button>
-                    </SimpleGrid>
-                </Box>
-                <Box bgColor="whiteAlpha.200" rounded="lg" py={2}>
-                    <SimpleGrid columns={3} spacing={4}>
-                        <Center>
-                            <Text align="center">ARDOR-5NCL-DRBZ-XBWF-DDN5T</Text>
-                        </Center>
-                        <Center>
-                            <Text align="center">Last message 12 minutes ago</Text>
-                        </Center>
-                        <Button variant="outline" mx={12}>Click to decrypt conversation</Button>
-                    </SimpleGrid>
-                </Box>
                 {messages.map((message, index) => (
-                    <Box key={index}>{message}</Box>
+                    <OneMessage key={index} message={message} />
                 ))}
             </Stack>
         </Box>
