70f5862ec02cc09e7c94a9b106c3edf7b37e4195
Mythical Beings <development@mythicalbeings.io>
2023-02-28 20:07:59 +0100

    BETA Book V1

--- src/components/Pages/BookPage/Book.js
+++ src/components/Pages/BookPage/Book.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { useState, forwardRef } from 'react';
+import { Box, Button, Center, Heading, useBreakpointValue, useMediaQuery } from '@chakra-ui/react';
 import HTMLFlipBook from 'react-pageflip';
 import { pdfjs, Document, Page as ReactPdfPage } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 import pdf from './Capitulo.pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const width = 540;
-const height = 763;
+const Page = forwardRef(({ pageNumber, width }, ref) => {
+    console.log('🚀 ~ file: Book.js:10 ~ Page ~ width:', width);
 
-const Page = React.forwardRef(({ pageNumber }, ref) => {
     return (
         <div ref={ref}>
             <ReactPdfPage pageNumber={pageNumber} width={width} />
@@ -18,18 +19,51 @@ const Page = React.forwardRef(({ pageNumber }, ref) => {
 });
 
 const Book = () => {
+    // ---------------------------------------------
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [isLargerThan800] = useMediaQuery('(min-width: 800px)');
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
+
+    const handleNextPage = () => {
+        if (pageNumber < numPages) setPageNumber(pageNumber + 1);
+    };
+
+    const handlePrevPage = () => {
+        if (pageNumber > 1) setPageNumber(pageNumber - 1);
+    };
+    // ---------------------------------------------
+    const width = useBreakpointValue({ base: 324, xl: 540 });
+    const height = useBreakpointValue({ base: 763, xl: 763 });
     return (
-        <>
+        <Box>
             <Heading>Book</Heading>
-            <Document file={pdf}>
-                <HTMLFlipBook width={width} height={height}>
-                    <Page pageNumber={1} />
-                    <Page pageNumber={2} />
-                    <Page pageNumber={3} />
-                    <Page pageNumber={4} />
-                </HTMLFlipBook>
-            </Document>
-        </>
+            {isLargerThan800 && (
+                <Document file={pdf}>
+                    <Center>
+                        <HTMLFlipBook width={width} height={height}>
+                            <Page pageNumber={1} width={width} />
+                            <Page pageNumber={2} width={width} />
+                            <Page pageNumber={3} width={width} />
+                            <Page pageNumber={4} width={width} />
+                        </HTMLFlipBook>
+                    </Center>
+                </Document>
+            )}
+
+            {!isLargerThan800 && (
+                <Box>
+                    <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+                        <ReactPdfPage pageNumber={pageNumber} width={340} />
+                    </Document>
+                    <Button w="50%" onClick={handlePrevPage}>Prev</Button>
+                    <Button w="50%" onClick={handleNextPage}>Next</Button>
+                </Box>
+            )}
+        </Box>
     );
 };
 
