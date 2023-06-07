e05a94551dafe20f32d2eb29c8c69ae406999a1c
JSanchezFDZ <info@jsanchezfdz.es>
2023-06-07 23:30:23 +0200

    Debounce

--- src/utils/omno.js
+++ src/utils/omno.js
@@ -1,12 +1,38 @@
 import axios from 'axios';
-// import { OMNO_API } from '../data/CONSTANTS';
+
+let cache = null;
+let isFetching = false;
 
 export const fetchOmnoMarket = async () => {
+    if (cache) {
+        console.log("🚀 ~ file: omno.js:8 ~ fetchOmnoMarket ~ cache:", cache)
+        return cache;
+    }
+
+    if (isFetching) {
+        console.log("🚀 ~ file: omno.js:13 ~ fetchOmnoMarket ~ isFetching:", isFetching)
+        // Si ya hay una solicitud en curso, esperamos a que termine y retornamos la caché actualizada
+        return new Promise((resolve) => {
+            const interval = setInterval(() => {
+                if (!isFetching) {
+                    clearInterval(interval);
+                    resolve(cache);
+                }
+            }, 300);
+        });
+    }
+
     try {
+        isFetching = true;
         const response = await axios.get('https://api.mythicalbeings.io/index.php?action=getOmnoMarket');
-        console.log("🚀 ~ file: omno.js:7 ~ fetchOmnoMarket ~ response:", response)
-        return response.data;
+        cache = response.data;
+        setTimeout(() => {
+            cache = null;
+        }, 7500);
+        return cache;
     } catch (error) {
         console.log('🚀 ~ file: omno.js:12 ~ fetchOmnoMarket ~ error:', error);
+    } finally {
+        isFetching = false;
     }
 };
