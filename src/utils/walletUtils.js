bb68f32bcbd495fd073146c09b9e381d056f0d89
JSanchezFDZ <info@jsanchezfdz.es>
2023-06-12 21:20:30 +0200

    FIX: wETH notification

--- src/utils/walletUtils.js
+++ src/utils/walletUtils.js
@@ -98,7 +98,14 @@ export const handleNotifications = ({
             const asset = getAsset(tx.attachment.asset, cards);
             const amount = Number(tx.attachment.quantityQNT);
 
-            if (asset && asset !== 'GEM' && asset !== 'GIFTZ' && asset !== 'wETH' && asset !== 'Unknown' && isIncoming) {
+            if (
+                asset &&
+                asset !== 'GEM' &&
+                asset !== 'GIFTZ' &&
+                asset !== 'WETH' &&
+                asset !== 'Unknown' &&
+                isIncoming
+            ) {
                 cardsForNotify.push({ asset, amount });
             } else {
                 handleConfirmateNotification(tx, isIncoming, toast, confirmedTransactionRef);
