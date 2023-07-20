ab8db66b5a13e584d7794dca6f489b243cbb6d3b
JSanchezFDZ <info@jsanchezfdz.es>
2023-07-20 19:00:44 +0200

    FIX: Get Jackpot balance in wETH

--- src/services/Jackpot/utils.js
+++ src/services/Jackpot/utils.js
@@ -1,17 +1,21 @@
-import { JACKPOTACCOUNT, JACKPOTHALF, NQTDIVIDER } from '../../data/CONSTANTS';
-import { getIgnisBalance } from '../Ardor/ardorInterface';
-import { getIgnisPrice } from '../coingecko/utils';
+import { JACKPOTACCOUNT, JACKPOTHALF, NQTDIVIDER, WETHASSETACCOUNT } from '../../data/CONSTANTS';
+import { fetchCurrencyAssets } from '../../utils/cardsUtils';
+// import { getIgnisBalance } from '../Ardor/ardorInterface';
+import { getEthPrice } from '../coingecko/utils';
 
 export const getJackpotBalance = async () => {
     // Recover Jackpot balance - IGNIS
-    const response = await getIgnisBalance(JACKPOTACCOUNT);
-    const balance = response.balanceNQT / NQTDIVIDER;
-    return (JACKPOTHALF ? balance / 2 : balance).toFixed(2);
+    //const response = await getIgnisBalance(JACKPOTACCOUNT);
+    const response = await fetchCurrencyAssets(JACKPOTACCOUNT, [WETHASSETACCOUNT], false);
+    const unconfirmedBalance = response[0][0].unconfirmedQuantityQNT;
+    const balance = unconfirmedBalance / NQTDIVIDER;
+    return (JACKPOTHALF ? balance / 2 : balance).toFixed(6);
 };
 
 export const getJackpotBalanceUSD = async jackpotBalance => {
-    const ignisPrice = await getIgnisPrice();
-    return (jackpotBalance * ignisPrice).toFixed(2);
+    // const ignisPrice = await getIgnisPrice();
+    const ethPrice = await getEthPrice();
+    return (jackpotBalance * ethPrice).toFixed(6);
 };
 
 export const getJackpotParticipants = async () => {
