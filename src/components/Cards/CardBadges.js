bfd1d60d2acf79e8750ba85bf8427b454a15b8af
JSanchezFDZ <info@jsanchezfdz.es>
2023-09-14 22:55:21 +0200

    Ana style

--- src/components/Cards/CardBadges.js
+++ src/components/Cards/CardBadges.js
@@ -2,7 +2,7 @@ import { Stack, Text, useColorModeValue } from '@chakra-ui/react';
 import { RARITY_COLORS } from '../../data/CONSTANTS';
 
 const CardBadges = ({ rarity, continent, size = 'md' }) => {
-    const badgeColor = useColorModeValue('linear-gradient(45deg, #8e9eab, #eef2f3) 1', 'linear-gradient(45deg, #8e9eab, #eef2f3) 1');
+    const continentColor = useColorModeValue('linear-gradient(45deg, #788188, #4a4c4d) 1', 'linear-gradient(45deg, #788188, #4a4c4d) 1');
 
     return (
         <Stack direction={{ base: 'column', md: 'row' }} spacing={1}>
@@ -18,13 +18,13 @@ const CardBadges = ({ rarity, continent, size = 'md' }) => {
                 {rarity}
             </Text>
             <Text
-                px={2}
+                px={3}
                 fontSize={size}
                 w={{ base: '6rem', md: 'auto' }}
                 h={{ base: '1.5rem', md: 'auto' }}
-                bgGradient={badgeColor}
+                bgGradient={continentColor}
                 rounded="lg"
-                color="black"
+                color="white"
                 textAlign="center">
                 {continent}
             </Text>
