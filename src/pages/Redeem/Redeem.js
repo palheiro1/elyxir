09cdfcbbf92cfe148f287bd8d771a18c265e6361
JSanchezFDZ <jsanchez@unknowngravity.com>
2024-05-27 16:24:41 +0100

    [FIX] Responsive + CurrencyMenu

--- src/pages/Redeem/Redeem.js
+++ src/pages/Redeem/Redeem.js
@@ -6,8 +6,8 @@ const Redeem = () => {
     return (
         <Box px={8}>
             <Center>
-                <Stack direction={{ base: 'column', lg: 'row' }} spacing={4} pt={8} align="center">
-                    <Box w="60%">
+                <Stack direction={{ base: 'column', lg: 'row' }} spacing={12} pt={8} align="center">
+                    <Box w={{ base: '90%', lg: '60%' }}>
                         <LoginButtons showLogIn={true} showNewUser={true} />
 
                         <Heading>
